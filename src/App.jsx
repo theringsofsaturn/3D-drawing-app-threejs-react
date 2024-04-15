0649211cb5be895aa5791378cbed132d24d2b6a0
theringsofsaturn <emiliankasemi@gmail.com>
2024-04-15 20:24:56 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { SketchApp } from "./components/SketchApp";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <SketchApp />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SketchApp.jsx
@@ -0,0 +1,5 @@
+import { Canvas } from "@react-three/fiber";
+
+export const SketchApp = () => {
+  return <Canvas></Canvas>;
+};
